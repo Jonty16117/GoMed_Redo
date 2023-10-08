1bd03476b6eddd36b015e55f7a141851a2c9a57b
Aryan Jain <ary45382@gmail.com>
2023-10-08 21:07:50 +0530

    Molly

--- aryan/frontend/src/components/Hello.js
+++ aryan/frontend/src/components/Hello.js
@@ -7,10 +7,14 @@
 // }
 
 // export default Hello;
-import { useState } from "react";
-import "./App.css";
 
-function App() {
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import "./Hello.css";
+
+const socket = io("http://localhost:5000"); 
+
+function Hello() {
   const [message, setMessage] = useState("");
   const [chats, setChats] = useState([]);
   const [isTyping, setIsTyping] = useState(false);
@@ -20,7 +24,6 @@ function App() {
 
     if (!message) return;
     setIsTyping(true);
-    scrollTo(0, 1e10);
 
     let msgs = chats;
     msgs.push({ role: "user", content: message });
@@ -28,6 +31,10 @@ function App() {
 
     setMessage("");
 
+    // Emit the user's message as a "question" event to the Socket.IO server
+    socket.emit("question", message);
+
+    // Fetch is used to send the message to the server (you can adjust this based on your needs)
     fetch("http://localhost:8000/", {
       method: "POST",
       headers: {
@@ -42,16 +49,30 @@ function App() {
         msgs.push(data.output);
         setChats(msgs);
         setIsTyping(false);
-        scrollTo(0, 1e10);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  // useEffect to handle answers from the Socket.IO server
+  useEffect(() => {
+    socket.on("answer", (data) => {
+      let msgs = chats;
+      msgs.push({ role: "server", content: data });
+      setChats(msgs);
+      setIsTyping(false);
+    });
+
+    // Clean up the socket connection on component unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, [chats]);
+
   return (
     <main>
-      <h1>FullStack Chat AI Tutorial</h1>
+      <h1 className="logo">GoMed-ChatBOT</h1>
 
       <section>
         {chats && chats.length
@@ -86,4 +107,4 @@ function App() {
   );
 }
 
-export default App;
+export default Hello;
